26223d1f16922065ccf05ea4345ce63b757a60aa
jlizarr1 <jlizarr1@jhu.edu>
2021-03-29 05:34:28 -0400

    added front end for log in

---
+++ elephant/src/components/SignupForms/StudentName.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+export default function StudentName() {
+  return (
+    <React.Fragment>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="grade"
+                label="Grade Level"
+                name="grade"
+                autoComplete="grade"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="country"
+                label="Country"
+                type="country"
+                id="country"
+              />
+            </Grid>
+        </Grid>
+    </React.Fragment>
+  );
+}
