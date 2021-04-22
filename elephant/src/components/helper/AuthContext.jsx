8846a6d4404d15fd3569002ac7d0819980cb87a8
jlizarr1 <jlizarr1@jhu.edu>
2021-04-22 18:58:57 -0400

    turned sign in into component, will have to do this with other react files to work with backend

---
+++ elephant/src/components/helper/AuthContext.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+
+export const AuthContext = React.createContext();
+
+function getCookieValue(a) {
+	let b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
+	return b ? b.pop() : null;
+}
+
+// this component will store the authentication status as well as the 
+// authenticated information that we need.
+export const AuthContextProvider = ({children}) => {
+	// check cookie for auth
+	// previous status of authentication
+	const prevName = getCookieValue("name") || null;
+	
+	// previous authentication information
+	const prevEmail = getCookieValue("email") || null;
+
+	const prevUserID = getCookieValue("userID") || null;
+
+	const prevProfilePic = getCookieValue("profilePic") || null;
+
+	const prevMentor = getCookieValue("isMentor") || null;
+
+	
+	const [name, setName] = useState(prevName);
+	const [email, setEmail] = useState(prevEmail);
+	const [userID, setUserID] = useState(prevUserID);
+	const [profilePic, setProfilePic] = useState(prevProfilePic);
+	const [isMentor, setMentor] = useState(prevMentor);
+
+	useEffect(
+		// this anonymous function will automatically update the cookies
+		() => {
+			document.cookie = "name=" + name + "; path=/";
+			document.cookie = "email=" + email + "; path=/";
+			document.cookie = "userID=" + userID + "; path=/";
+			document.cookie = "profilePic=" + profilePic +"; path=/";
+			document.cookie = "isMentor=" + isMentor +"; path=/";
+		},
+		[name, email, userID, profilePic, isMentor]
+	)
+	
+	const defaultContext = {
+		name,
+		setName,
+		email,
+		setEmail,
+		userID,
+		setUserID,
+		profilePic,
+		setProfilePic,
+		isMentor,
+		setMentor
+	}
+
+	return (
+		<AuthContext.Provider value={defaultContext}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
