19a2e64c44348d569317ebf1ace7852542fde886
jlizarr1 <jlizarr1@jhu.edu>
2021-05-01 15:48:44 -0400

    small change for deployment

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from app import create_app
 from config import ProdConfig
 
 application = create_app(ProdConfig)
-application.run(debug=True)
+if __name__ == "__main__":
+    application.run(debug=True)
