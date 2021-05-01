7a34a503c949027e5703b6c8dc85c01f6010aab5
jlizarr1 <jlizarr1@jhu.edu>
2021-05-01 15:04:12 -0400

    made changes to connect to online database

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ class TestConfig(object):
 
 
 class ProdConfig(object):
-    MONGO_DBNAME = "proddb"
-    MONGO_URI = "mongodb://localhost:27017/" + MONGO_DBNAME
+    """ MONGO_DBNAME = "proddb"
+    MONGO_URI = "mongodb://localhost:27017/" + MONGO_DBNAME """
+    MONGO_URI = "mongodb+srv://jlizarr1:fuzRZbivOaaxj5Va@cluster0.qlruk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
